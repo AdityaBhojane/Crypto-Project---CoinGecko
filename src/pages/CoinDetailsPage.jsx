9459a53a2e169dbdf779c98fcbc36d408a86c684
aditya bhojane <adityabhojane66@gmail.com>
2024-08-18 00:27:19 +0530

    add update

--- src/pages/CoinDetailsPage.jsx
+++ src/pages/CoinDetailsPage.jsx
@@ -1,8 +1,17 @@
+import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
+import { fetchCoinData } from "../services/fetchCoinData";
 
 function CoinDetailsPage() {
 
     const { coinId } = useParams();
+
+    const { data, isLoading, isError, error} = useQuery(['coins', page, currency], () => fetchCoinData(page, currency), {
+        // retry: 2,
+        // retryDelay: 1000,
+        cacheTime: 1000 * 60 * 2,
+        staleTime: 1000 * 60 * 2,
+    });
 
     return (
         <div>
